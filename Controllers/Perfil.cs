cc9a0d30da8598c8870d100bceb897e3c75d01a2
Antonio Fava <afavajrbr80@gmail.com>
2021-07-27 08:02:58 -0400

    first commit

---
+++ Controllers/Perfil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using AppLogin.ViewModels;
+
+namespace AppLogin.Controllers
+{
+    public class Perfil : Controller
+    {
+        // GET: Perfil
+        [Authorize]
+        public ActionResult AlterarSenha()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult AlterarSenha(AlterarSenhaViewModel viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var identity = User.Identity as ClaimsIdentity;
+            var login = identity.Claims.FirstOrDefault(c => c.Type == "Login").Value;
+
+            var usuario = db.Usuarios.FirstOrDefault(u => u.Login == login);
+
+            if (Hash.GerarHash(viewmodel.SenhaAtual) != usuario.Senha)
+            {
+                ModelState.AddModelError("SenhaAtual", "Senha incorreta");
+                return View();
+            }
+
+            usuario.Senha = Hash.GerarHash(viewmodel.NovaSenha);
+            db.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Painel");
+        }
+    }
+}
